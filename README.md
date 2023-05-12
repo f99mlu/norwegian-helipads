00e60f1333e6b9b52e92a1a061314ea100005e19
f99mlu <mats.lundquist75@gmail.com>
2023-05-12 19:17:16 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,20 @@
+# Norwegian helipads
+Helipads/heliports in Norway for MSFS.
+
+## Install Git
+https://git-scm.com/download/win
+
+## Create the local folder
+mkdir c:\git-repo
+
+## Clone the project
+git clone https://github.com/f99mlu/norwegian-helipads.git
+
+## Build the project
+Edit build.bat and customize your SDK and Community folder paths
+
+## Run the project
+Double-click on build.bat
+
+## Make changes to the helipads
+Edit the file PackageSources\scenery\helipads.xml and run build.bat to try it out.
---
+++ build.bat
@@ -0,0 +1,11 @@
+cd "C:\MSFS SDK\Tools\bin"
+rmdir /s /q c:\Temp\Packages\f99mlu-norway-helipads
+fspackagetool.exe "c:\Server\git-repo\norwegian-helipads\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
+rmdir /s /q e:\MSFS\Community\f99mlu-norway-helipads
+xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads e:\MSFS\Community\f99mlu-norway-helipads
+
+rem rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
+rem xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
+
+
+rem xcopy /E /I c:\git-repo\norwegian-helipads\PackageSources\modelLib %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads\scenery\modelLib
