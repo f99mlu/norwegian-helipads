69089771e89d40bb5961ddb35f8e8eca01e999b2
f99mlu <mats.lundquist75@gmail.com>
2023-07-10 11:27:55 +0200

    Update build paths

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 cd "C:\MSFS SDK\Tools\bin"
 rmdir /s /q c:\Temp\Packages\f99mlu-norway-helipads
-fspackagetool.exe "c:\Server\git-repo\norwegian-helipads\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
+fspackagetool.exe "D:\git-repos\norwegian-helipads\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
 copy c:\Temp\Packages\f99mlu-norway-helipads\layout.json c:\Temp\Packages\f99mlu-norway-helipads\layout_noconflicts.json
-rmdir /s /q e:\MSFS\Community\f99mlu-norway-helipads
-xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads e:\MSFS\Community\f99mlu-norway-helipads
+rmdir /s /q D:\MSFS\Community\f99mlu-norway-helipads
+xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads D:\MSFS\Community\f99mlu-norway-helipads
 
 rem rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
 rem xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
