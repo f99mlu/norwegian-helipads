ed44b49f650f30f6d326a633b062dd3f001a7d7e
f99mlu <mats.lundquist75@gmail.com>
2024-11-20 23:22:49 +0100

    Add MSFS 2024 build script

--- build.bat
+++ build.bat
@@ -4,9 +4,3 @@ fspackagetool.exe "D:\git-repos\norwegian-helipads\f99mlu-norway-helipadsproject
 copy c:\Temp\Packages\f99mlu-norway-helipads\layout.json c:\Temp\Packages\f99mlu-norway-helipads\layout_noconflicts.json
 rmdir /s /q D:\MSFS\Community\f99mlu-norway-helipads
 xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads D:\MSFS\Community\f99mlu-norway-helipads
-
-rem rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
-rem xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads
-
-
-rem xcopy /E /I c:\git-repo\norwegian-helipads\PackageSources\modelLib %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-norway-helipads\scenery\modelLib
