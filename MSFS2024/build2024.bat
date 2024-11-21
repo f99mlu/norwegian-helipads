485f9bfc02c5e28bc50d53347f457e2feb5f0d24
f99mlu <mats.lundquist75@gmail.com>
2024-11-21 22:49:22 +0100

    MSFS2024 stuff

--- MSFS2024/build2024.bat
+++ MSFS2024/build2024.bat
@@ -1,5 +1,5 @@
 cd "C:\MSFS 2024 SDK\Tools\bin"
 rmdir /s /q c:\Temp\Packages\f99mlu-norway-helipads
-fspackagetool.exe "D:\git-repos\norwegian-helipads\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
+fspackagetool.exe "D:\git-repos\norwegian-helipads\MSFS2024\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
 rmdir /s /q E:\MSFS2024\Community\f99mlu-norway-helipads
 xcopy /E /I c:\Temp\Packages\f99mlu-norway-helipads E:\MSFS2024\Community\f99mlu-norway-helipads
