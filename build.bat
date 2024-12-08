e684894b934aae2b227bf0e88a0b7cb6e9b96915
f99mlu <mats.lundquist75@gmail.com>
2024-12-08 11:47:00 +0100

    Add no conflicts calc to build script

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 cd "C:\MSFS SDK\Tools\bin"
+java -jar ..\RemoveXML.jar a d:\git-repos\norwegian-helipads\PackageSources\scenery\helipads.xml d:\git-repos\norwegian-helipads\PackageSources\scenery\helipads_noconflicts.xml 68.77 67.65 60.76 58.16 59.74 60.379 60.373 10.37 10.71 \"5.35 \"5.34 \"5.31 \"6.31
 rmdir /s /q c:\Temp\Packages\f99mlu-norway-helipads
 fspackagetool.exe "D:\git-repos\norwegian-helipads\f99mlu-norway-helipadsproject.xml" -outputdir c:\temp
 java -jar ..\RemoveXML.jar b c:\Temp\Packages\f99mlu-norway-helipads\layout.json c:\Temp\Packages\f99mlu-norway-helipads\layout_noconflicts.json helipads.bgl
